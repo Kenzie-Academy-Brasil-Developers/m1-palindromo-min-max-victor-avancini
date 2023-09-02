5d265586ae63283168895c340faf847fd1ec4001
victor-avancini <victor.avancini@hotmail.com>
2023-09-02 12:29:41 -0300

    Função 'isPalindrome' desenvolvida

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    let frase = str.toUpperCase().replaceAll(" ","");
+    let fraseContraria = "";
+    for(let i = 0; i < str.length; i++){
+        fraseContraria = (fraseContraria + str[str.length - 1-i]).toUpperCase().replaceAll(" ","");
+    }
+    if (frase == fraseContraria) {
+        return true
+    }
+    return false
 }
+console.log(isPalindrome('A mala nada na lama'))
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
